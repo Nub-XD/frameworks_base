6e34c73057b36283debc319372a1fd1bcad1c4a4
madmax7896 <madmax78968@gmail.com>
2022-12-29 23:12:42 +0530

    Revert "base: Introduce custom face unlock implementation [1/2]"
    This reverts commit 0d52716e1eb5b449dc76c652f4d8f12b4c5335c9.

--- packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
@@ -765,9 +765,6 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
 
     private void handleFingerprintAuthenticated(int authUserId, boolean isStrongBiometric) {
         Trace.beginSection("KeyGuardUpdateMonitor#handlerFingerPrintAuthenticated");
-        if (mOccludingAppRequestingFace){
-            requestFaceAuthOnOccludingApp(false);
-        }
         try {
             final int userId;
             try {
@@ -977,9 +974,6 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
 
     private void handleFaceAuthenticated(int authUserId, boolean isStrongBiometric) {
         Trace.beginSection("KeyGuardUpdateMonitor#handlerFaceAuthenticated");
-        if (mOccludingAppRequestingFace){
-            requestFaceAuthOnOccludingApp(false);
-        }
         try {
             if (mGoingToSleep) {
                 Log.d(TAG, "Aborted successful auth because device is going to sleep.");
